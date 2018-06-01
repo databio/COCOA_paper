9a60e901dc284cbf3c5cd10afa1aa4c9e6871efa
j-lawson <jtl2hk@virginia.edu>
2018-06-01 17:47:22 -0400

    move code loading regions into a separate script

--- src/PCA_Region_Set_Enrichment_Analysis.R
+++ src/PCA_Region_Set_Enrichment_Analysis.R
@@ -41,53 +41,7 @@ trainingMData = brcaMList[["methylProp"]][,
 ###########################################################
 # reading in the region sets
 # load LOLA database
-lolaPath1 = paste0(Sys.getenv("RESOURCES"), "/regions/LOLACore/hg38/")
-regionSetDB = loadRegionDB(lolaPath1)
-loRegionAnno = regionSetDB$regionAnno
-lolaCoreRegionAnno = loRegionAnno
-# a549Ind = grep("a549", loRegionAnno$cellType, ignore.case = TRUE)
-sheff_dnaseInd = grep("sheffield_dnase", loRegionAnno$collection, ignore.case = TRUE)
-# mcf7Ind = grep("mcf-7", loRegionAnno$cellType, ignore.case = TRUE)
-# k562Ind = grep("k562", loRegionAnno$cellType,  ignore.case = TRUE)
-# GRList = GRangesList(regionSetDB$regionGRL[c(a549Ind, mcf7Ind)])
-fInd1 = filterFetal(lolaCoreRegionAnno)
-lolaCoreRegionAnno = lolaCoreRegionAnno[-sort(unique(c(sheff_dnaseInd, fInd1)))]
-GRList1 = GRangesList(regionSetDB$regionGRL[-sort(unique(c(sheff_dnaseInd, fInd1)))])
-
-# ROADMAP Epigenome project and Jaspar motifs
-lolaPath2 = paste0(Sys.getenv("RESOURCES"), "/regions/LOLAExt/hg38/")
-regionSetDB2 = loadRegionDB(lolaPath2, useCache = TRUE)
-loRegionAnno2 = regionSetDB2$regionAnno
-roadmapRegionAnno = loRegionAnno2[loRegionAnno2$collection == "roadmap_epigenomics", ]
-GRList2 = GRangesList(regionSetDB2$regionGRL[loRegionAnno2$collection == "roadmap_epigenomics"])
-fInd2 = filterFetal(roadmapRegionAnno)
-roadmapRegionAnno = roadmapRegionAnno[-fInd2]
-GRList2 = GRList2[-fInd2]
-
-# processing Jaspar motif regions
-# resizing regions and filtering to only open chromatin in K562
-# size was ~999
-motifRegionAnno = loRegionAnno2[loRegionAnno2$collection == "jaspar_motifs", ]
-GRList3 = GRangesList(regionSetDB2$regionGRL[loRegionAnno2$collection == "jaspar_motifs"])
-# filtering based on chromatin accessibility data from blood/AML
-# load(paste0(Sys.getenv("PROCESSED"), "/aml_e3999/prjResources/","bloodAccessibleRegions.RData"))
-# GRList3 = getOLRegions(GRList = GRList3, intGR=bloodAccessibleRegions, removeOL = FALSE)
-# GRList3 = GRangesList(GRList3)
-#I'm not sure that center is where motif is: GRList3 = resize(GRList3, width = 200, fix="center") 
-
-# combine into one GRList
-GRList = c(GRList1, GRList2, GRList3)
-
-#adding ER Chipseq dataset
-erSet = fread(paste0(Sys.getenv("CODE"), "PCARegionAnalysis/inst/extdata/",
-                     "GSM2305313_MCF7_E2_peaks_hg38.bed"))
-setnames(erSet, c("V1", "V2", "V3"), c("chr", "start", "end"))
-GRList = c(GRangesList(MIRA:::dtToGr(erSet)), GRList)
-# names(GRList) = c("GSM2305313_MCF7_E2_peaks_hg38.bed", names(GRList))
-
-#################################################################
-# cleaning up since there were many large objects
-rm(list = c("GRList1", "GRList2", "GRList3", "regionSetDB", "regionSetDB2"))
+source(paste0(Sys.getenv("CODE"), "pcrsa_method_paper/R/load_process_regions_brca.R"))
 
 #################################################################
 
