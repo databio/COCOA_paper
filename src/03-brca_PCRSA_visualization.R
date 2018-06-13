46dc0dc798353bd6713557f0d09ab78cf7fb0d67
j-lawson <jtl2hk@virginia.edu>
2018-06-13 17:00:40 -0400

    Scripts calling PCRSA visualization pipeline (set up parameters)

--- src/03-brca_PCRSA_visualization.R
+++ src/03-brca_PCRSA_visualization.R
@@ -1,2 +1,52 @@
 # 
 # PCsToAnnotate_cPCH = c("PC1m4", "PC1p3", PCSTOANNOTATE)
+
+source(paste0(Sys.getenv("CODE"),"pcrsa_method_paper/src/00-init.R"))
+Sys.setenv("PLOTS"=paste0(Sys.getenv("PROCESSED"), "brca_PCA/analysis/plots/"))
+########################################
+source(paste0(Sys.getenv("CODE"), "/pcrsa_method_paper/src/load_process_regions_brca.R"))
+
+#################################################################
+
+# parameters for PCRSA_vis_pipeline.R
+plotSubdir = "03_brca_PCRSA_Vis"
+# a cache is created in the script
+setCacheDir(paste0(Sys.getenv("PROCESSED"), "brca_PCA/RCache/")) 
+simpleCache("combinedBRCAMethyl_noXY", assignToVariable = "bigSharedC", reload = TRUE)
+# screen out patients without ER/PGR status
+methylData = bigSharedC[["methylProp"]][, 
+                                          colnames(bigSharedC[["methylProp"]]) %in% patientMetadata[, subject_ID]] 
+
+# GRList # from load_process_regions pipeline
+coordinateDT = bigSharedC$coordinates
+allMPCAString = "allMPCA_657"
+simpleCache(allMPCAString, assignToVariable = "mPCA", reload = TRUE)
+loadingMat = mPCA$rotation
+# use rsEnString to specify?
+simpleCache("rsEnrichment_657", assignToVariable = "rsEnrichment", reload = TRUE)
+simpleCache("rsEnrichmentTop10_657", assignToVariable = "rsEnrichmentTop10", reload = TRUE)
+# TODO make sure GRList and rsEnrichment are both in the same order/with same data
+names(GRList) <- paste0(rsEnrichment$rsName, " : ", rsEnrichment$rsDescription)
+GRList = GRList[!is.na(rsEnrichment$PC1)]
+rsEnrichment=rsEnrichment[!is.na(rsEnrichment$PC1), ]
+rsEnSortedInd= rsRankingIndex(rsEnrichment = rsEnrichment, PCsToAnnotate = paste0("PC", 1:10))
+
+rsEnrichmentTop10 = rsEnrichmentTop10[!is.na(rsEnrichmentTop10$PC1), ]
+PCSTOANNOTATE = paste0("PC", 1:10)
+
+
+### plots that will be created and script specific parameters for them 
+# "comparePCHeatmap"
+PCsToAnnotate_cPCH = PCSTOANNOTATE
+# "methylAlongPC"
+topRSToPlotNum = 15
+PCsToAnnotate_mAPC = PCSTOANNOTATE[1:5]
+# "regionQuantileByPC"
+PCsToAnnotate_rQBPC = PCSTOANNOTATE
+topRSInd_rQBPC = unique(unlist(rsEnSortedInd[1:15, ])) # get top region sets from each PC
+# pcFromSubset Correlation Heatmap
+PCsToAnnotate_pcFSCH = PCSTOANNOTATE
+topRSInd_pcFSCH = unique(unlist(rsEnSortedInd[1:15, ])) # get top region sets from each PC
+
+# the pipeline
+source("PCRSA_vis_pipeline.R") 
--- src/53-ewing_PCRSA_visualization.R
+++ src/53-ewing_PCRSA_visualization.R
@@ -1,42 +1,48 @@
+# Plots for ewing_PCRSA_visualization
 
-
-
+source(paste0(Sys.getenv("CODE"),"pcrsa_method_paper/src/00-init.R"))
 ########################################
-
 source(paste0(Sys.getenv("CODE"), "/pcrsa_method_paper/src/load_process_regions_brca.R"))
 
 #################################################################
 
 # parameters for PCRSA_vis_pipeline.R
 plotSubdir = "53_Ewing_PCRSA_Vis"
+# a cache is created in the script
 setCacheDir(paste0(Sys.getenv("PROCESSED"), "ews_patients/RCache/")) 
-rsEnSortedInd
-# GRList # from load_process_regions pipeline 
-coordinateDT = brcaMList$coordinates
-loadingMat = allMPCA$rotation
-methylData = brcaMList$methylProp
-allMPCAString 
-simpleCache(allMPCAString, assignToVariable = "mPCA")
+simpleCache("bigSharedC_pQC", assignToVariable = "bigSharedC", reload = TRUE)
+
+# GRList # from load_process_regions pipeline
+coordinateDT = bigSharedC$coordinates
+methylData = bigSharedC$methylProp
+allMPCAString = "allMPCA2"
+simpleCache(allMPCAString, assignToVariable = "mPCA", reload = TRUE)
+loadingMat = mPCA$rotation
 # use rsEnString to specify?
-simpleCache("rsEnrichment_657", assignToVariable = "rsEnrichment")
-simpleCache("rsEnrichmentTop10_657", assignToVariable = "rsEnrichmentTop10")
+simpleCache("rsEnrichment") # , assignToVariable = "rsEnrichment")
+simpleCache("rsEnrichmentTop10") # , assignToVariable = "rsEnrichmentTop10")
+# TODO make sure GRList and rsEnrichment are both in the same order/with same data
+names(GRList) <- paste0(rsEnrichment$rsName, " : ", rsEnrichment$rsDescription)
+GRList = GRList[!is.na(rsEnrichment$PC1)]
+rsEnrichment=rsEnrichment[!is.na(rsEnrichment$PC1), ]
+rsEnSortedInd= rsRankingIndex(rsEnrichment = rsEnrichment, PCsToAnnotate = paste0("PC", 1:10))
 
+rsEnrichmentTop10 = rsEnrichmentTop10[!is.na(rsEnrichmentTop10$PC1), ]
 PCSTOANNOTATE = paste0("PC", 1:10)
-
-
 
 
 ### plots that will be created and script specific parameters for them 
 # "comparePCHeatmap"
 PCsToAnnotate_cPCH = PCSTOANNOTATE
 # "methylAlongPC"
+topRSToPlotNum = 15
 PCsToAnnotate_mAPC = PCSTOANNOTATE[1:5]
 # "regionQuantileByPC"
-PCsToAnnotate_rQBPC = c("PC1m4", "PC1p3", PCSTOANNOTATE)
-topRSInd_rQBPC = unique(unlist(rsEnSortedInd[1:10, ])) # get top 10 region sets from each PC
+PCsToAnnotate_rQBPC = PCSTOANNOTATE
+topRSInd_rQBPC = unique(unlist(rsEnSortedInd[1:15, ])) # get top region sets from each PC
 # pcFromSubset Correlation Heatmap
-PCsToAnnotate_pcFSCHM = PCSTOANNOTATE
-topRSInd_pcFSCHM = unique(unlist(rsEnSortedInd[1:10, ])) # get top 10 region sets from each PC
+PCsToAnnotate_pcFSCH = PCSTOANNOTATE
+topRSInd_pcFSCH = unique(unlist(rsEnSortedInd[1:15, ])) # get top region sets from each PC
 
 # the pipeline
 source("PCRSA_vis_pipeline.R") 
