ef8586debed191422df03b6a17bce22c51f82c7d
j-lawson <jtl2hk@virginia.edu>
2018-07-05 16:03:36 -0400

    Updates to Ewing PC figure

--- src/60-ewing_pca_visualization.R
+++ src/60-ewing_pca_visualization.R
@@ -1,4 +1,5 @@
 plotSubdir = "/"
+library(ggplot2)
 
 grDevices::pdf(paste0(Sys.getenv("PLOTS"), plotSubdir,"ewing_PC_plots.pdf"))
 PCsToAnnotate = paste0("PC", 1:10)
@@ -18,4 +19,7 @@ grDevices::pdf(paste0(Sys.getenv("PLOTS"), plotSubdir,"ewing_PC1_PC2.pdf"))
 
 plot(mPCA$x[, "PC1"], mPCA$x[, "PC2"], xlab="PC1", ylab="PC2")
 
-dev.off()
+pcDF = data.frame(PC1=mPCA$x[, "PC1"], PC2 = mPCA$x[, "PC2"])
+
+pc1_2 = ggplot(data = pcDF, mapping = aes(x=PC1, y=PC2)) + geom_point(size=3) + theme_classic()
+ggsave(paste0(Sys.getenv("PLOTS"), plotSubdir,"ewing_PC_plots.pdf"))
