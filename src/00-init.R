03cfd803029bb468940d97f03fc0ec552f750ad8
j-lawson <jtl2hk@virginia.edu>
2019-02-01 14:30:11 -0500

    Figures for paper

--- src/00-init.R
+++ src/00-init.R
@@ -30,3 +30,22 @@ patientMetadata = brcaMetadata
 process_brca_expr = function(exprDF) {
     
 }
+
+
+setSubPlotDir = function(dirName) {
+    Sys.setenv("SUBPLOTDIR"=paste0(dirName, "/"))
+}
+
+dirPlot = function(plotFile) {
+    if (!is.na(Sys.getenv("SUBPLOTDIR", unset = NA))) {
+        return(paste0(Sys.getenv("PLOTS"), Sys.getenv("SUBPLOTDIR"), plotFile))    
+    } else(
+        return(paste0(Sys.getenv("PLOTS"), plotFile))
+    )
+    
+}
+
+
+# set environment
+Sys.setenv("PLOTS"=paste0(Sys.getenv("PROCESSED"), "COCOA_paper/analysis/plots/"))
+setCacheDir(paste0(Sys.getenv("PROCESSED"), "COCOA_paper/RCache/"))
---
+++ src/15-brca_CLL_figures.R
@@ -0,0 +1,47 @@
+# library(projectInit)
+
+# project.init(codeRoot = paste0(Sys.getenv("CODE"), "PCARegionAnalysis/R/"), dataDir = paste0(Sys.getenv("PROCESSED"), "brca_PCA/"))
+source(paste0(Sys.getenv("CODE"), "COCOA_paper/src/00-init.R"))
+
+setwd(paste0(Sys.getenv("PROCESSED"), "COCOA_paper/analysis/"))
+patientMetadata = brcaMetadata # already screened out patients with incomplete ER or PGR mutation status
+# there should be 657 such patients
+set.seed(1234)
+
+
+#############################################################################
+simpleCache("rsEnrichmentRSMean_657", assignToVariable = "rsScores")
+
+# summary figure of COCOA BRCA results: ER set relative ranking among region sets
+rsScores$origInd = 1:nrow(rsScores)
+rsScores = rsScores[order(rsScores$PC1, decreasing = TRUE), ]
+erIndPC1 = grepl(pattern = "esr1|eralpha", x = rsScores$rsName, ignore.case = TRUE)
+erIndPC1 = erIndPC1 | grepl(pattern = "esr1|eralpha", x = rsScores$rsDescription, ignore.case = TRUE)
+rsScores$rsDescription[erIndPC1]
+plot(erIndPC1)
+erIndPC1 = which(erIndPC1)
+hist(erIndPC1, breaks = seq(0, 2000, by=200))
+
+plotRSConcentration <- function(rsScores, scoreColName="PC1", 
+                                colsToSearch = c("rsName", "rsDescription"), 
+                                pattern, breaks) {
+    
+    rsScores = as.data.frame(rsScores)
+    rsScores = rsScores[order(rsScores[, scoreColName], decreasing = TRUE), ]
+    
+    rsInd = rep(FALSE, nrow(rsScores))
+    for (i in seq_along(colsToSearch)) {
+        rsInd = rsInd | grepl(pattern = pattern, x = rsScores[, colsToSearch[i]], ignore.case = TRUE)
+    }
+    
+    rsInd = which(rsInd)
+    hist(rsInd, breaks = breaks)
+    
+}
+
+plotRSConcentration(rsScores=rsScores, scoreColName = "PC5", pattern = "esr1|eralpha", breaks=seq(0, 2000, by=200))
+plotRSConcentration(rsScores=rsScores, 
+                    scoreColName = "PC5", 
+                    pattern = "esr1|eralpha", 
+                    breaks=seq(0, 2000, by=200))
+
